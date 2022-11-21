66a4839d36942459452827aa54c7e957e6057876
Diegohofs <diegohofs@gmail.com>
2022-11-21 19:33:39 -0300

    in progress

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,73 +1,39 @@
+import { useContext } from "react";
+import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 
 export function History() {
-    return (
-      <HistoryContainer>
-        <h1>Meu histórico</h1>
+  const { cycles } = useContext(CyclesContext)
 
-        <HistoryList>
-          <table>
-            <thead>
-              <tr>
-                <th>Tarefa</th>
-                <th>Duração</th>
-                <th>Início</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"yellow"}>Em andamento</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"red"}>Interrompido</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td><Status statusColor={"green"}>Concluído</Status></td>
-              </tr>
-            </tbody>
-          </table>
-        </HistoryList>
-      </HistoryContainer>
-    );
-  }
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map(cycle => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td><Status statusColor={"green"}>Concluído</Status></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
